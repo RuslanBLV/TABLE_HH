05e674a961f4b5df9a2de928df72a26be96d047f
Ruslan <rus.beluaev.101@gmail.com>
2025-05-25 15:04:21 +0700

    1

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,8 +1,6 @@
 import psycopg2
-import requests
-from src.hh_vacansy import (result_list, name_company_alfa, name_company_vtb, name_company_efin, name_company_psb,
-                            name_company_tbank, name_company_sovcombank, name_company_pao, name_company_bsk,
-                            name_company_otp, name_company_bks)
+from typing import List, Tuple, Union
+from src.hh_vacansy import (result_list_vacancies, result_list_employees)
 
 conn = psycopg2.connect(
     host='localhost',
@@ -17,18 +15,30 @@ cur.execute(f"""
     DROP TABLE vacancies;
     CREATE TABLE vacancies (
         vacancies_id SERIAL PRIMARY KEY,
-        company_id int,
-        company VARCHAR,
         vacancies VARCHAR(255),
         salary int,
         currency VARCHAR,
         url VARCHAR
     );
 """)
+cur.execute(f"""
+    DROP TABLE employees;
+    CREATE TABLE employees (
+        employees_id SERIAL PRIMARY KEY,
+        company VARCHAR
+    );
+""")
 conn.commit()
 
 
 class DBManager:
+    with conn.cursor() as cursor:
+        cursor.executemany("INSERT INTO employees (employees_id, company) VALUES (%s, %s)", result_list_employees)
+        cursor.execute("ALTER TABLE vacancies ADD COLUMN employees_id INTEGER REFERENCES employees(employees_id)")
+        cursor.executemany("INSERT INTO vacancies (vacancies_id, vacancies, salary, currency, url, employees_id) "
+                               "VALUES (%s, %s, %s, %s, %s, %s)", result_list_vacancies)
+        conn.commit()
+
     def __init__(self, host='localhost', database='test', user='postgres', password='12345'):
         self.conn = psycopg2.connect(
             host=host,
@@ -37,54 +47,48 @@ class DBManager:
             password=password
         )
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(self) -> List[Tuple[int, str, Union[int, None], str, str, str]]:
+        # получает список всех вакансий с указанием названия компании,
+        # названия вакансии и зарплаты и ссылки на вакансию.
         with self.conn.cursor() as cursor:
-            cursor.executemany("INSERT INTO vacancies (vacancies_id, company_id, vacancies, salary, currency, url) "
-                               "VALUES (%s, %s, %s, %s, %s, %s)", result_list)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 80", name_company_alfa)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 4181", name_company_vtb)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 8643040", name_company_efin)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 6591", name_company_psb)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 78638", name_company_tbank)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 7944", name_company_sovcombank)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 89", name_company_pao)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 3003159", name_company_bsk)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 4394", name_company_otp)
-            cursor.executemany("UPDATE vacancies SET company = %s WHERE company_id = 1833", name_company_bks)
-            self.conn.commit()
-            cursor.execute("SELECT * FROM vacancies")
+            cursor.execute("SELECT vacancies.vacancies_id, vacancies.vacancies, vacancies.salary, vacancies.currency,"
+                           " vacancies.url, employees.company AS company FROM vacancies JOIN employees "
+                           "ON vacancies.employees_id = employees.employees_id;")
             rows = cursor.fetchall()
             return rows
 
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self) -> List[Tuple[str, int]]:
+        # получает список всех компаний и количество вакансий у каждой компании
         with self.conn.cursor() as cursor:
-            cursor.execute("SELECT company, COUNT(*) AS vacancies FROM vacancies "
-                           "GROUP BY company")
+            cursor.execute("SELECT employees.company, COUNT(*) AS vacancies FROM vacancies JOIN employees "
+                           "ON vacancies.employees_id = employees.employees_id GROUP BY company")
             self.conn.commit()
-
             rows = cursor.fetchall()
             return rows
 
-    def get_avg_salary(self):
+    def get_avg_salary(self) -> List[Tuple[Union[float, None]]]:
+        # получает среднюю зарплату по вакансиям
         with self.conn.cursor() as cursor:
             cursor.execute('SELECT AVG(salary) AS avg_salary FROM vacancies')
             self.conn.commit()
-
             rows = cursor.fetchall()
             return rows
 
-    def get_vacancies_with_higher_salary(self):
+    def get_vacancies_with_higher_salary(self) -> List[Tuple]:
+        # получает список всех вакансий, у которых зарплата выше средней по всем вакансиям
         with self.conn.cursor() as cursor:
             cursor.execute("SELECT * FROM vacancies WHERE salary::numeric > (SELECT AVG(salary::numeric) "
                            "FROM vacancies)")
             self.conn.commit()
-
             rows = cursor.fetchall()
             return rows
 
+    def get_vacancies_with_keyword(self) -> List[Tuple]:
+        #  получает список всех вакансий, в названии которых содержатся переданные в метод слова
+        keyword = input("Введите название вакансии для поиска: ")
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM vacancies WHERE vacancies LIKE '{keyword}%'")
+            self.conn.commit()
+            rows = cursor.fetchall()
+            return rows
 
-i = DBManager()
-print(i.get_all_vacancies())
-print(i.get_companies_and_vacancies_count())
-print(i.get_avg_salary())
-print(i.get_vacancies_with_higher_salary())
